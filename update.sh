689e047bbdc9e58b5b488ceeb30d3072b79d4816
john-walks-slow <john.ren.contact@gmail.com>
2020-05-08 14:29:09 +0800

    Update posts 2020-05-08

--- update.sh
+++ update.sh
@@ -1,22 +1,22 @@
 source_dir="/mnt/d/OneDrive - The Chinese University of Hong Kong/2. Writing"
+target_dir="_posts"
 
 git checkout source
-rm -rf _posts/*
-cp -r "$source_dir"/* _posts/
+rm -rf $target_dir/*
 
 today=$(date +'%Y-%m-%d')
 
 cd _posts
 today=$(date +'%Y-%m-%d')
 
-for folder in source_dir; do
+for folder in $source_dir; do
     if [ -d $folder ] ; then
         cd $folder
         for file in *; do
             if [[ $file =~ ^.*@publish.md$ ]]; then
                 date=$(date -r "$file" "+%Y-%m-%d")
                 name="$date-$folder | ${file// /-}"
-                mv "$file" "${name%@publish.*}.md"
+                mv "$file" "$target_dir/${name%@publish.*}.md"
             fi
         done
         cd ..
