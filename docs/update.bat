671e69d3ffbed76fd00ae3b9dd8e97993b614220
JohnnRen <john.ren.contact@gmail.com>
2023-12-01 23:05:39 +0800

    Update articles 2023/12/01

--- docs/update.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set REPO_DIR=%CD%
-@REM set SOURCE_DIR=%REPO_DIR%\_drafts
-@REM set TARGET_DIR=%REPO_DIR%\_posts
-set today=%date:~0,4%/%date:~5,2%/%date:~8,2%
-
-git pull
-jekyll build --destination docs && git add . && git commit -m "Update articles %today%" && git push
